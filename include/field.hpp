8890c4cf964ab4ba3c2b71199b60469fc189c321
zannabianca1997 <zannabianca199712@gmail.com>
2016-04-01 17:29:32 +0200

    Moved boundaries struct

--- include/field.hpp
+++ include/field.hpp
@@ -3,23 +3,24 @@
 
 #include "celltype.hpp"
 
-
 namespace cells
 {
+  // i limiti di un campo
+  struct boundaries {
+      float xmin, xmax;
+      float ymin, ymax;
+  };
 
   //array class
   class field {
   public:
-    field(unsigned char); //constructor, log_dim è il logaritmo in base 2 del lato
+    field(unsigned char, boundaries); //constructor, log_dim è il logaritmo in base 2 del lato
                           //max log_dim = 15
     unsigned int lenght; //il numero di celle
     unsigned int index(unsigned int, unsigned int); //l'indice alla cella x,y
     unsigned int side;
 
-    struct {
-      float xmin, xmax;
-      float ymin, ymax;
-    } boundaries; // i limiti della simulazione 
+    boundaries limits; // i limiti della simulazione 
 
     cell items; // i dati veri e propri
     inline cell* get_start_cell() { return CloneCell(&items); }; //return copy of start cell
